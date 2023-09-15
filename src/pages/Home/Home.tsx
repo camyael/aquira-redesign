ba5fcbed1fae23c63ff5df81bfee78aa05109152
Camila Villaverde <camilavillaverde3@gmail.com>
2023-09-15 18:38:26 -0300

    first commit

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,11 @@
+import LandingPage from "../../components/LandingPage/LandingPage";
+
+const Home = () => {
+    return (
+        <section id="Home">
+            <LandingPage/>
+        </section>
+    )
+}
+
+export default Home;
