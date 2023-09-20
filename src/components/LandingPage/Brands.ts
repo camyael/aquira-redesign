ef530f32a4ae8fe28913aa056218f3f9805a0057
Camila Villaverde <camilavillaverde3@gmail.com>
2023-09-19 22:36:46 -0300

    creating landing page & adding branding

---
+++ src/components/LandingPage/Brands.ts
@@ -0,0 +1,35 @@
+interface Brand {
+    name : string,
+    img : string,
+    link : string
+}
+
+const Brands : Brand[] = [
+    {
+        "name" : "La Roche-Posay",
+        "img" : "la-roche-posay",
+        "link" : "https://www.laroche-posay.com.ar/"
+    },
+    {
+        "name" : "Glossier",
+        "img" : "glossier",
+        "link" : "https://www.glossier.com/"
+    },
+    {
+        "name" : "Dior",
+        "img" : "dior",
+        "link" : "https://www.dior.com/"
+    },
+    {
+        "name" : "Maybelline",
+        "img" : "maybelline",
+        "link" : "https://www.maybelline.com.ar/"
+    },
+    {
+        "name" : "Cerave",
+        "img" : "cerave",
+        "link" : "https://www.cerave.com.ar/"
+    }
+]
+
+export default Brands;
--- src/components/LandingPage/LandingPage.tsx
+++ src/components/LandingPage/LandingPage.tsx
@@ -1,8 +1,34 @@
+import './LandingPage.css';
+import home from '../../assets/home.webp';
+import Brands from './Brands';
+
 const LandingPage = () => {
     return (
-        <div>
-            
-        </div>
+        <section id='landing-page'>
+            <div className='landing-page_presentation'>
+                <div>
+                    <p>Cuidados para tu piel</p>
+                    <div className='presentation-buttons'>
+                        <button className='button-buyNow'>Comprar ahora</button>
+                        <button className='button-readMore'>Leer más</button>
+                    </div>
+                </div>
+                <img src={home} alt="" />
+            </div>
+            <div className='landing-page_brands'>
+                {
+                    Brands.map((brand, index) => {
+                        return (
+                            <div key={index}>
+                                <a href={brand.link} target='_blank'>
+                                    <img src={`../src/assets/brands/${brand.img}.webp`} alt={brand.name} />
+                                </a>
+                            </div>
+                        )
+                    })
+                }
+            </div>
+        </section>
     )
 }
 
